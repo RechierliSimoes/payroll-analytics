8a2498dcd576e8c59299a43702bfac2c5e458227
RechierliSimoes <rechierliwork14@outlook.com>
2026-01-04 19:58:58 -0300

    Registra Company no Django Admin

--- app/payrolls/admin.py
+++ app/payrolls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Payroll
 
-# Register your models here.
+
+@admin.register(Payroll)
+class PayrollAdmin(admin.ModelAdmin):
+    list_display = ("company", "reference_month", "reference_year", "status", "created_at")
+    list_filter = ("status", "reference_year")
+    search_fields = ("company__name",)
